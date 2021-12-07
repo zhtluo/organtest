741af57e57ac5b3c6f0c29b161e8fa5bad6c6074
zhtluo <zhtluo@gmail.com>
2021-12-07 13:15:49 -0500

    Base and bulk

--- benches/mult.rs
+++ benches/mult.rs
@@ -52,7 +52,7 @@ fn multithread_compute(param: CompParameters) {
     }
 }
 
-pub fn criterion_benchmark(c: &mut Criterion) {
+pub fn criterion_benchmark_bulk(c: &mut Criterion) {
     let mut xs = vec![1, 4]
         .into_iter()
         .map(Integer::from)
@@ -66,19 +66,23 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         .collect::<Vec<_>>();
     assert_eq!(xs, ys_ex);
 
-    let len = 13; // 2^len = 8196
-    let p = Integer::from_str_radix("615318156686744305707043916358806301172428827813979806125310234541438764580027412187489137576809400194366963713", 10).unwrap();
-    // (262 * (2 ** 360)) + 1
-    let w = {
-        let mut tmp = Integer::from_str_radix("262", 10).unwrap();
-        let mut lnd = 360 - 13;
-        while lnd >= 0 {
-            tmp.square_mut();
-            tmp %= &p;
-            lnd -= 1;
-        }
-        tmp
-    };
+    /*
+    BULK_PARAMS = {
+        'P': PBULK,
+        'Q': QBULK,
+        'RING_V': (7 * (2 ** 290)) + 1,
+        'VECTOR_LENGTH': 8192,
+        'BITS': 226
+    }
+    PBULK = 2 ** 226 - 5
+    QBULK = int(group256.order())
+    group256: secp256k1
+    https://neuromancer.sk/std/secg/secp256k1
+        */
+
+    let len = 13; // 2^len = 8192
+    let p: Integer = Integer::from(Integer::u_pow_u(2, 226)) - 5;
+    let w = Integer::from(Integer::u_pow_u(2, 290)) * 7 + 1;
     let mut rand = RandState::new();
     let mut input1: Vec<Integer> = repeat_with(|| p.clone().random_below(&mut rand))
         .take(1 << len)
@@ -89,7 +93,7 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     bit_rev_radix_2_ntt(&mut input1, &p, &w);
     bit_rev_radix_2_ntt(&mut input2, &p, &w);
     let zq_order = Integer::from_str_radix(
-        "01ffffffffffffffffffffffffffffffffffe9ae2ed07577265dff7f94451e061e163c61",
+        "fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141",
         16,
     )
     .unwrap();
@@ -102,8 +106,65 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         order: zq_order,
     };
 
-    c.bench_function("compute", |b| b.iter(|| multithread_compute(param.clone())));
+    c.bench_function("bulk_compute", |b| {
+        b.iter(|| multithread_compute(param.clone()))
+    });
 }
 
-criterion_group!(benches, criterion_benchmark);
+pub fn criterion_benchmark_base(c: &mut Criterion) {
+    let mut xs = vec![1, 4]
+        .into_iter()
+        .map(Integer::from)
+        .collect::<Vec<_>>();
+    let p = Integer::from(7);
+    let w = Integer::from(6);
+    bit_rev_radix_2_ntt(&mut xs, &p, &w);
+    let ys_ex = vec![5, 4]
+        .into_iter()
+        .map(Integer::from)
+        .collect::<Vec<_>>();
+    assert_eq!(xs, ys_ex);
+
+    /*
+    BASE_PARAMS = {
+        'P': 2 ** 32 - 5,
+        #'Q': 2 ** 56 - 5,
+        'Q': group112.order(),
+
+        'RING_V': (57 * (2 ** 96)) + 1,
+        'VECTOR_LENGTH': 2048,
+        'BITS': 32
+    }
+    group112 : secp112r1
+    https://neuromancer.sk/std/secg/secp112r1
+        */
+
+    let len = 11; // 2^len = 2048
+    let p: Integer = Integer::from(Integer::u_pow_u(2, 32)) - 5; // 2 ** 32 - 5
+    let w = Integer::from(Integer::u_pow_u(2, 96)) * 57 + 1;
+    let mut rand = RandState::new();
+    let mut input1: Vec<Integer> = repeat_with(|| p.clone().random_below(&mut rand))
+        .take(1 << len)
+        .collect();
+    let mut input2: Vec<Integer> = repeat_with(|| p.clone().random_below(&mut rand))
+        .take(1 << len)
+        .collect();
+    bit_rev_radix_2_ntt(&mut input1, &p, &w);
+    bit_rev_radix_2_ntt(&mut input2, &p, &w);
+    let zq_order = Integer::from_str_radix("db7c2abf62e35e7628dfac6561c5", 16).unwrap();
+
+    let param = CompParameters {
+        a: input1,
+        b: input2,
+        p: p,
+        w: w,
+        order: zq_order,
+    };
+
+    c.bench_function("base_compute", |b| {
+        b.iter(|| multithread_compute(param.clone()))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark_base, criterion_benchmark_bulk);
 criterion_main!(benches);
